1e1664ea4d88949c7cb605495567b7bafe046835
Takashi Hayakawa <chou_22@icloud.com>
2022-02-25 22:31:39 +0900

    Introduce tailwind

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,17 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>buyme</title>
       </Head>
 
       <main>
-        <h1>lets start create buyme</h1>
+        <h1 className="text-3xl font-bold">Let`s start create buyme</h1>
       </main>
     </div>
   );
